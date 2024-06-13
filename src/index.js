9b6d71067317bb4b2d7b3fef3d8659410f0f7e61
Gustavo de la Torre <gdlt88@gmail.com>
2024-06-13 14:53:57 -0600

    adding validation around variables displayed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const jsforce = require('jsforce');
+const jsforce = require("jsforce");
 require("dotenv").config({ path: "./.env" });
 
 const username = process.env.USERNAME;
@@ -7,19 +7,27 @@ const securityToken = process.env.SECURITY_TOKEN;
 const LOGIN_URL = process.env.LOGIN_URL;
 const TOPIC = process.env.TOPIC;
 
-
-const conn = new jsforce.Connection({loginUrl : LOGIN_URL});
-conn.login(username, password + securityToken, function(err, res) {
-  if (err) { 
-      return console.error(err);
+const conn = new jsforce.Connection({ loginUrl: LOGIN_URL });
+conn.login(username, password + securityToken, function (err, res) {
+  if (err) {
+    return console.error(err);
   }
 
-  console.log('Authenticated');
-  
-  conn.streaming.topic(TOPIC).subscribe(function(message) {
-    console.log('Event Type : ' + message.event.type);
-    console.log('Event Created : ' + message.event.createdDate);
-    console.log('Object Id : ' + message.sobject.Id);
-    console.log('Event : ' + JSON.stringify(message));
+  console.log("Authenticated");
+
+  conn.streaming.topic(TOPIC).subscribe(function (message) {
+    if (message.event !== undefined && message.event.type !== undefined) {
+      console.log("Event Type : " + message.event.type);
+    }
+    if (
+      message.event !== undefined &&
+      message.event.createdDate !== undefined
+    ) {
+      console.log("Event Created : " + message.event.createdDate);
+    }
+    if (message.sobject !== undefined && message.sobject.Id !== undefined) {
+      console.log("Object Id : " + message.sobject.Id);
+    }
+    console.log("Event : " + JSON.stringify(message));
   });
 });
